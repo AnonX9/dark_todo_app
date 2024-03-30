79d361039289618f5126a15958e9de2cfa1184ae
Nametissa <152576363+AnonX9@users.noreply.github.com>
2024-03-30 02:01:28 +0100

    Initialize with_easy_l10n

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod_starter/clients/connection_status_listener.dart';
+import 'package:flutter_riverpod_starter/clients/local_db.client.dart';
+import 'package:flutter_riverpod_starter/core/app_entry.dart';
+import 'package:flutter_riverpod_starter/logs/app_provider_observer.dart';
+import 'package:stack_trace/stack_trace.dart';
+
+import 'clients/talker.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await EasyLocalization.ensureInitialized();
+
+  final objectBox =
+      await ObjectBoxManager.init(); // Initialize ObjectBoxManager
+
+  FlutterError.onError = (details) => talker.handle(
+        details.exception,
+        switch (details.stack) {
+          final value? => Trace.from(value).terse,
+          _ => null,
+        },
+        details.exceptionAsString(),
+      );
+
+  await initNoInternetListener();
+
+  runApp(
+    ProviderScope(
+      overrides: [localDbProvider.overrideWithValue(objectBox)],
+      observers: [AppProviderObserver(talker)],
+      child: EasyLocalization(
+        supportedLocales: [Locale('en'), Locale('fr')],
+        path: 'assets/translations',
+        fallbackLocale: Locale('en'),
+        child: AppEntry(),
+      ),
+    ),
+  );
+}
